341b185fe0d711871588cb8123080e5662cb0bfe
Emma <94088311+emmatinney@users.noreply.github.com>
2025-06-24 16:13:59 -0400

    new changes

--- site/js/custom_login 2.js
+++
@@ -1,4 +0,0 @@
-// Redirect to login page if not logged in and current page is not login page
-if (!localStorage.getItem('loggedIn') && !window.location.pathname.includes('/login')) {
-    window.location.href = 'login.html';
-}
